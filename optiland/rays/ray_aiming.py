7db91e3318613c7d532e0d989ddf9940896f299b
Kramer <kdanielharrison@gmail.com>
2024-12-10 15:27:34 -0600

    feat: add RayAimer classes and factory for ray generation

---
+++ optiland/rays/ray_aiming.py
@@ -0,0 +1,35 @@
+from abc import ABC
+
+
+class RayAimer(ABC):
+
+    def __init__(self, optic):
+        self.optic = optic
+
+
+class BisectionRayAimer(RayAimer):
+
+    def __init__(self, optic):
+        super().__init__(optic)
+
+    def generate_rays(self, Hx, Hy, Px, Py, initial_rays):
+        pass
+
+
+class NewtonRayAimer(RayAimer):
+
+    def __init__(self, optic):
+        super().__init__(optic)
+
+    def generate_rays(self, Hx, Hy, Px, Py, initial_rays):
+        pass
+
+
+class RayAimerFactory:
+
+    @staticmethod
+    def create(aiming_type, optic):
+        if aiming_type == 'bisection':
+            return BisectionRayAimer(optic)
+        else:
+            raise ValueError('Invalid aiming type: {}'.format(aiming_type))
