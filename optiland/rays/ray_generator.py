031bb24848529eaf99a03fe46e7b49e9a5237a64
Kramer <kdanielharrison@gmail.com>
2024-12-10 18:15:22 -0600

    feat: refactor ray generation with FieldCalculator and factory pattern

--- optiland/rays/ray_generator.py
+++ optiland/rays/ray_generator.py
@@ -1,12 +1,15 @@
+from abc import ABC, abstractmethod
+import pickle
+import hashlib
 import numpy as np
 from optiland.rays.real_rays import RealRays
 from optiland.rays.polarized_rays import PolarizedRays
+from optiland.rays.ray_cache import RayCache
+from optiland.rays.ray_aiming import RayAimerFactory
 
 
-class RayGenerator:
-    """
-    Generator class for creating rays.
-    """
+class FieldCalculator(ABC):
+
     def __init__(self, optic):
         self.optic = optic
 
@@ -51,6 +54,9 @@ class RayGenerator:
             y1 = Py * EPD * vy / 2
             z1 = np.full_like(Px, EPL)
 
+        return self._build_ray_instance(x0, y0, z0, x1, y1, z1, wavelength)
+
+    def _build_ray_instance(self, x0, y0, z0, x1, y1, z1, wavelength):
         mag = np.sqrt((x1 - x0)**2 + (y1 - y0)**2 + (z1 - z0)**2)
         L = (x1 - x0) / mag
         M = (y1 - y0) / mag
@@ -70,6 +76,51 @@ class RayGenerator:
             return RealRays(x0, y0, z0, L, M, N, intensity, wavelength)
         else:
             return PolarizedRays(x0, y0, z0, L, M, N, intensity, wavelength)
+
+    @abstractmethod
+    def _get_ray_origins(self, Hx, Hy, Px, Py, vx, vy):
+        """
+        Calculate the initial positions for rays originating at the object.
+
+        Args:
+            Hx (float): Normalized x field coordinate.
+            Hy (float): Normalized y field coordinate.
+            Px (float or np.ndarray): x-coordinate of the pupil point.
+            Py (float or np.ndarray): y-coordinate of the pupil point.
+            vx (float): Vignetting factor in the x-direction.
+            vy (float): Vignetting factor in the y-direction.
+
+        Returns:
+            tuple: A tuple containing the x, y, and z coordinates of the
+                object position.
+
+        Raises:
+            ValueError: If the field type is "object_height" for an object at
+                infinity.
+
+        """
+        pass  # pragma: no cover
+
+    def _get_starting_z_offset(self):
+        """
+        Calculate the starting ray z-coordinate offset for systems with an
+        object at infinity. This is relative to the first surface of the optic.
+
+        This method chooses a starting point that is equivalent to the entrance
+        pupil diameter of the optic.
+
+        Returns:
+            float: The z-coordinate offset relative to the first surface.
+        """
+        z = self.optic.surface_group.positions[1:-1]
+        offset = self.optic.paraxial.EPD()
+        return offset - np.min(z)
+
+
+class AngleFieldCalculator(FieldCalculator):
+
+    def __init__(self, optic):
+        super().__init__(optic)
 
     def _get_ray_origins(self, Hx, Hy, Px, Py, vx, vy):
         """
@@ -97,9 +148,6 @@ class RayGenerator:
         field_x = max_field * Hx
         field_y = max_field * Hy
         if obj.is_infinite:
-            if self.optic.field_type == 'object_height':
-                raise ValueError('Field type cannot be "object_height" for an '
-                                 'object at infinity.')
             if self.optic.obj_space_telecentric:
                 raise ValueError('Object space cannot be telecentric for an '
                                  'object at infinity.')
@@ -117,16 +165,10 @@ class RayGenerator:
             y0 = Py * EPD / 2 * vy + y
             z0 = np.full_like(Px, z)
         else:
-            if self.optic.field_type == 'object_height':
-                x = field_x
-                y = field_y
-                z = obj.geometry.sag(x, y) + obj.geometry.cs.z
-
-            elif self.optic.field_type == 'angle':
-                EPL = self.optic.paraxial.EPL()
-                z = self.optic.surface_group.positions[0]
-                x = np.tan(np.radians(field_x)) * (EPL - z)
-                y = -np.tan(np.radians(field_y)) * (EPL - z)
+            EPL = self.optic.paraxial.EPL()
+            z = self.optic.surface_group.positions[0]
+            x = np.tan(np.radians(field_x)) * (EPL - z)
+            y = -np.tan(np.radians(field_y)) * (EPL - z)
 
             x0 = np.full_like(Px, x)
             y0 = np.full_like(Px, y)
@@ -134,17 +176,111 @@ class RayGenerator:
 
         return x0, y0, z0
 
-    def _get_starting_z_offset(self):
-        """
-        Calculate the starting ray z-coordinate offset for systems with an
-        object at infinity. This is relative to the first surface of the optic.
 
-        This method chooses a starting point that is equivalent to the entrance
-        pupil diameter of the optic.
+class ObjectHeightFieldCalculator(FieldCalculator):
+
+    def __init__(self, optic):
+        super().__init__(optic)
+
+    def _get_ray_origins(self, Hx, Hy, Px, Py, vx, vy):
+        """
+        Calculate the initial positions for rays originating at the object.
+
+        Args:
+            Hx (float): Normalized x field coordinate.
+            Hy (float): Normalized y field coordinate.
+            Px (float or np.ndarray): x-coordinate of the pupil point.
+            Py (float or np.ndarray): y-coordinate of the pupil point.
+            vx (float): Vignetting factor in the x-direction.
+            vy (float): Vignetting factor in the y-direction.
 
         Returns:
-            float: The z-coordinate offset relative to the first surface.
+            tuple: A tuple containing the x, y, and z coordinates of the
+                object position.
         """
-        z = self.optic.surface_group.positions[1:-1]
-        offset = self.optic.paraxial.EPD()
-        return offset - np.min(z)
+        obj = self.optic.object_surface
+        max_field = self.optic.fields.max_field
+        field_x = max_field * Hx
+        field_y = max_field * Hy
+
+        # TODO: use object coordinate system
+        x = field_x
+        y = field_y
+        z = obj.geometry.sag(x, y) + obj.geometry.cs.z
+
+        x0 = np.full_like(Px, x)
+        y0 = np.full_like(Px, y)
+        z0 = np.full_like(Px, z)
+
+        return x0, y0, z0
+
+
+class FieldCalculatorFactory:
+
+    @staticmethod
+    def create(optic):
+        if optic.field_type == 'angle':
+            return AngleFieldCalculator(optic)
+        elif optic.field_type == 'object_height':
+            return ObjectHeightFieldCalculator(optic)
+        else:
+            raise ValueError('Invalid field type: {}'.format(optic.field_type))
+
+
+class RayGenerator:
+
+    def __init__(self, optic):
+        self.optic = optic
+        self.aiming_type = None
+        self.ray_aimer = None
+        self.field_calculator = FieldCalculatorFactory.create(optic)
+        self.cache = RayCache()
+
+    def generate_rays(self, Hx, Hy, Px, Py, wavelength):
+        if self.ray_aimer:
+            # Generate a unique key for the ray cache
+            key = self._generate_cache_key(Hx, Hy, Px, Py, wavelength)
+
+            # Check if the rays are already in the cache
+            rays = self.cache.get_rays(key)
+            if rays:
+                return rays
+
+            # Generate the initial rays for aiming
+            aim_key = (Hx, Hy, Px, Py, wavelength)
+            initial_rays = self.ray_aimer.aim_rays(aim_key)
+
+            # If the initial rays are not found, generate new rays
+            if not initial_rays:
+                initial_rays = self.field_calculator.generate_rays(
+                    Hx, Hy, Px, Py, wavelength)
+
+            # Generate the final rays
+            rays = self.ray_aimer.generate_rays(Hx, Hy, Px, Py, initial_rays)
+
+            # Add the rays to the cache
+            self.cache.add_rays(key, rays)
+
+        else:
+            rays = self.field_calculator.generate_rays(
+                Hx, Hy, Px, Py, wavelength)
+
+        return rays
+
+    def set_ray_aiming(self, aiming_type):
+        self.aiming_type = aiming_type
+        self.ray_aimer = RayAimerFactory.create(self.optic, aiming_type)
+
+    def _generate_object_hash(self):
+        # Serialize the system object
+        serialized_sys = pickle.dumps(self.optic)
+
+        # Create a hash of the serialized data
+        hash_obj = hashlib.sha256(serialized_sys)
+
+        # Return the hexadecimal representation of the hash
+        return hash_obj.hexdigest()
+
+    def _generate_cache_key(self, Hx, Hy, Px, Py, wavelength):
+        system_hash = self._generate_object_hash()
+        return (system_hash, self.aiming_type, Hx, Hy, Px, Py, wavelength)
