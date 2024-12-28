0e57c69436127bdd5dea753b854d0b36d579deb5
Kramer <kdanielharrison@gmail.com>
2024-12-28 15:27:39 -0600

    feat: Refactor device management to improve device setting and retrieval

--- optiland/device.py
+++ optiland/device.py
@@ -1,13 +1,24 @@
 import torch
 
 
-_device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
-def set_device(device: str):
-    global _device
-    _device = device
+def set_device(device_name: str):
+    """
+    Set the global device to 'cpu' or 'cuda'.
+
+    Args:
+        device_name: The device name. Choose from 'cpu' or 'cuda'.
+    """
+    global device
+    if device_name == "cuda" and not torch.cuda.is_available():
+        raise ValueError("CUDA is not available on this system.")
+    device = torch.device(device_name)
 
 
 def get_device():
-    return _device
+    """
+    Get the current global device.
+    """
+    return device
