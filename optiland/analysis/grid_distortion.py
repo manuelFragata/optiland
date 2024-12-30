1b31ecfba6634e102109dbb01afde81cf4b240cb
Kramer <kdanielharrison@gmail.com>
2024-12-29 21:11:32 -0600

    feat: Refactor GridDistortion to use backend functions and prepare data for visualization

--- optiland/analysis/grid_distortion.py
+++ optiland/analysis/grid_distortion.py
@@ -6,8 +6,9 @@ for an optical system.
 
 Kramer Harrison, 2024
 """
-import numpy as np
+# import numpy as np
 import matplotlib.pyplot as plt
+import optiland.backend as be
 
 
 class GridDistortion:
@@ -52,13 +53,16 @@ class GridDistortion:
             figsize (tuple, optional): The size of the figure.
                 Defaults to (7, 5.5).
         """
+        # prepare data for visualization
+        data = self._prepare_data()
+
         fig, ax = plt.subplots(figsize=figsize)
 
-        ax.plot(self.data['xp'], self.data['yp'], 'C1', linewidth=1)
-        ax.plot(self.data['xp'].T, self.data['yp'].T, 'C1', linewidth=1)
+        ax.plot(data['xp'], data['yp'], 'C1', linewidth=1)
+        ax.plot(data['xp'].T, data['yp'].T, 'C1', linewidth=1)
 
-        ax.plot(self.data['xr'], self.data['yr'], 'C0P')
-        ax.plot(self.data['xr'].T, self.data['yr'].T, 'C0P')
+        ax.plot(data['xr'], data['yr'], 'C0P')
+        ax.plot(data['xr'].T, data['yr'].T, 'C0P')
 
         ax.set_xlabel('Image X (mm)')
         ax.set_ylabel('Image Y (mm)')
@@ -67,7 +71,7 @@ class GridDistortion:
         ax.spines['right'].set_visible(False)
         ax.spines['top'].set_visible(False)
 
-        max_distortion = self.data['max_distortion']
+        max_distortion = data['max_distortion']
         ax.set_title(f'Max Distortion: {max_distortion:.2f}%')
         fig.tight_layout()
         plt.show()
@@ -86,20 +90,20 @@ class GridDistortion:
         self.optic.trace_generic(Hx=0, Hy=1e-10, Px=0, Py=0,
                                  wavelength=self.wavelength)
 
-        max_field = np.sqrt(2) / 2
-        extent = np.linspace(-max_field, max_field, self.num_points)
-        Hx, Hy = np.meshgrid(extent, extent)
+        max_field = be.sqrt(be.array(2.0)) / 2
+        extent = be.linspace(-max_field, max_field, self.num_points)
+        Hx, Hy = be.meshgrid(extent, extent)
 
         if self.distortion_type == 'f-tan':
             const = (self.optic.surface_group.y[-1, 0] /
-                     (np.tan(1e-10 * np.radians(self.optic.fields.max_field))))
-            xp = const * np.tan(Hx * np.radians(self.optic.fields.max_field))
-            yp = const * np.tan(Hy * np.radians(self.optic.fields.max_field))
+                     (be.tan(1e-10 * be.deg2rad(self.optic.fields.max_field))))
+            xp = const * be.tan(Hx * be.deg2rad(self.optic.fields.max_field))
+            yp = const * be.tan(Hy * be.deg2rad(self.optic.fields.max_field))
         elif self.distortion_type == 'f-theta':
             const = (self.optic.surface_group.y[-1, 0] /
-                     (1e-10 * np.radians(self.optic.fields.max_field)))
-            xp = const * Hx * np.radians(self.optic.fields.max_field)
-            yp = const * Hy * np.radians(self.optic.fields.max_field)
+                     (1e-10 * be.deg2rad(self.optic.fields.max_field)))
+            xp = const * Hx * be.deg2rad(self.optic.fields.max_field)
+            yp = const * Hy * be.deg2rad(self.optic.fields.max_field)
         else:
             raise ValueError('''Distortion type must be "f-tan" or
                                 "f-theta"''')
@@ -110,20 +114,35 @@ class GridDistortion:
         data = {}
 
         # make real grid square for ease of plotting
-        data['xr'] = np.reshape(self.optic.surface_group.x[-1, :],
+        data['xr'] = be.reshape(self.optic.surface_group.x[-1, :],
                                 (self.num_points, self.num_points))
-        data['yr'] = np.reshape(self.optic.surface_group.y[-1, :],
+        data['yr'] = be.reshape(self.optic.surface_group.y[-1, :],
                                 (self.num_points, self.num_points))
 
         # optical system flips x, so must correct this
-        data['xp'] = np.flip(xp)
+        data['xp'] = be.flip(xp)
         data['yp'] = yp
 
         # Find max distortion
-        delta = np.sqrt((data['xp'] - data['xr'])**2 +
+        delta = be.sqrt((data['xp'] - data['xr'])**2 +
                         (data['yp'] - data['yr'])**2)
-        rp = np.sqrt(data['xp']**2 + data['yp']**2)
+        rp = be.sqrt(data['xp']**2 + data['yp']**2)
 
-        data['max_distortion'] = np.max(100 * delta / rp)
+        data['max_distortion'] = be.max(100 * delta / rp)
 
+        return data
+
+    def _prepare_data(self):
+        """
+        Prepares the data for visualization.
+
+        Returns:
+            dict: The prepared data.
+        """
+        data = {}
+        data['xr'] = be.to_numpy(self.data['xr'])
+        data['yr'] = be.to_numpy(self.data['yr'])
+        data['xp'] = be.to_numpy(self.data['xp'])
+        data['yp'] = be.to_numpy(self.data['yp'])
+        data['max_distortion'] = be.to_numpy(self.data['max_distortion'])
         return data
