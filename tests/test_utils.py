3c7fc9fbbc20b1909c9a418917aa6de24f4b2951
Kramer <kdanielharrison@gmail.com>
2025-01-08 10:45:29 -0600

    refactor: Simplify tensor conversion in assert_allclose using to_numpy

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,14 +1,11 @@
 import numpy as np
-import torch
+from optiland.backend import to_numpy
 
 
 def assert_allclose(a, b, atol=1e-6, rtol=1e-5):
     """Assert that two arrays or tensors are element-wise equal within
     tolerance.
     """
-    # Convert inputs to NumPy arrays
-    if isinstance(a, torch.Tensor):
-        a = a.detach().cpu().numpy()
-    if isinstance(b, torch.Tensor):
-        b = b.detach().cpu().numpy()
+    a = to_numpy(a)
+    b = to_numpy(b)
     assert np.allclose(a, b, atol=atol, rtol=rtol)
