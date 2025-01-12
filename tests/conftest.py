edaaf9c4207c674cb7e993ef3ddc8c892d1b876a
Kramer <kdanielharrison@gmail.com>
2025-01-12 20:42:06 +0100

    refactor: remove global tolerance variables and simplify assert_allclose function

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,5 @@
 import pytest
-import numpy as np
 import optiland.backend as be
-
-
-# Tolerance for comparing values in tests
-_atol = 1e-9
-_rtol = 1e-8
-
-
-def assert_allclose(a, b, atol=_atol, rtol=_rtol):
-    """Assert that two arrays or tensors are element-wise equal within
-    tolerance.
-    """
-    a = be.to_numpy(a)
-    b = be.to_numpy(b)
-    assert np.allclose(a, b, atol=atol, rtol=rtol)
 
 
 @pytest.fixture(
@@ -24,24 +9,17 @@ def backend(request):
     """Fixture to set the backend for each test and ensure proper device
     configuration.
     """
-    global _atol, _rtol
-
     backend_name = request.param
     be.set_backend(backend_name)
 
     if backend_name == 'torch':
-        # torch uses 32-bit floats by default,
-        # so we need to adjust the tolerance
-        _atol = 1e-6
-        _rtol = 1e-5
         be.set_device('cpu')  # Use CPU for tests
         be.grad_mode.disable()  # Disable gradient tracking
+
     yield
 
     # Reset the backend to numpy after the test
     be.set_backend('numpy')
-    _atol = 1e-9
-    _rtol = 1e-8
 
 
 def pytest_configure(config):
